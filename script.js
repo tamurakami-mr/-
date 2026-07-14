5738e552a41f683d7d43f099c7f9c618edb8bcf8
tamurakami-mr <tamurakami@bltinc.co.jp>
2026-07-14 23:54:42 +0900

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function updateTime() {
+    const now = new Date(); 
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+
+    hours = hours < 10 ? '0' + hours : hours;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+
+    document.getElementById('real-time').textContent = hours + ':' + minutes;
+}
+
+updateTime();
+
+setInterval(updateTime, 1000);
+
+const areaSelect = document.getElementById('area-select');
+const mapFrame = document.getElementById('map-frame');
+
+areaSelect.addEventListener('change', function() {
+    mapFrame.src = areaSelect.value;
+});
+
