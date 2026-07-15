3dcaa7b2e42044af6d9a671d103d110b908b1c60
tamurakami-mr <tamurakami@bltinc.co.jp>
2026-07-15 16:15:56 +0900

    カメラロール追加
    カメラロールの画像を追加しました。
    script.jsの変更
    index.html,stylesheet.cssの変更

--- script.js
+++ script.js
@@ -22,3 +22,40 @@ if (areaSelect && mapFrame) {
     });
 }
 
+const images = [
+  "cameraroll/C01.JPG",
+  "cameraroll/C02.JPG",
+  "cameraroll/C03.JPG",
+  "cameraroll/C04.JPG",
+  "cameraroll/C05.JPG",
+  "cameraroll/C06.JPG",
+  "cameraroll/C07.JPG",
+  "cameraroll/C08.JPG",
+  "cameraroll/C09.JPG",
+  "cameraroll/C10.JPG",
+  "cameraroll/C11.JPG",
+];
+
+let currentIndex = 0;
+
+const slideImage = document.getElementById('slideshow');
+
+setInterval(function() {
+
+slideImage.style.opacity = 0;
+  
+setTimeout(function() {
+
+currentIndex = currentIndex + 1;
+  
+if (currentIndex >= images.length) {
+    currentIndex = 0;
+}
+
+slideImage.src = images[currentIndex];
+
+slideImage.style.opacity = 1;
+
+},500);
+
+}, 5000);
